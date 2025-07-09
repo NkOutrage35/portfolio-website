2b5b7fd353a5ea3addecf2fc895ac4790e4f4106
Agyemang Kwame <kwameagyemang2007@gmail.com>
2025-07-09 03:04:03 +0100

    my_project and resource section styling.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-document.getElementById('current-year').textContent = new Date().getFullYear();
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleButton');
-    const navLinks = document.getElementById('navLinks');
+document.getElementById("current-year").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
+  const navLinks = document.getElementById("navLinks");
 
-    toggleButton.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-        toggleButton.classList.toggle('active'); // Add this line to toggle the button's active state
-    });
+  toggleButton.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+    toggleButton.classList.toggle("active");
+  });
 });
-if (document.querySelector('.typed')) {
-        var typed = new Typed('.typed', {
-            strings: ["I'm Ayemang Kwame Antwi.", "A Junior Web Developer.", "A Student at AAMUST."],
-            typeSpeed: 50,
-            backSpeed: 30,
-            loop: true
-        });
-    }
+if (document.querySelector(".typed")) {
+  var typed = new Typed(".typed", {
+    strings: [
+      "I'm Ayemang Kwame Antwi.",
+      "A Junior Web Developer.",
+      "A Student at AAMUST.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+  });
+}
